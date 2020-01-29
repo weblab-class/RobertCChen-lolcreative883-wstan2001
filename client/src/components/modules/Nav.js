92d10d95e1381caeee7b5ad0c3e256ad26d496f1
Robert Chen <robertchenschool@gmail.com>
2020-01-29 04:53:23 -0500

    moved login button to right

--- client/src/components/modules/Nav.js
+++ client/src/components/modules/Nav.js
@@ -37,23 +37,25 @@ class Nav extends Component {
                         Profile
                         </Link>
                     )}
-                    {this.props.userId ? (
-                        <GoogleLogout
-                        clientId={GOOGLE_CLIENT_ID}
-                        buttonText="Logout"
-                        onLogoutSuccess={this.props.handleLogout}
-                        onFailure={(err) => console.log(err)}
-                        className="NavBar-link NavBar-login"
-                        />
-                    ) : (
-                        <GoogleLogin
-                        clientId={GOOGLE_CLIENT_ID}
-                        buttonText="Login"
-                        onSuccess={this.props.handleLogin}
-                        onFailure={(err) => console.log(err)}
-                        className="NavBar-link NavBar-login"
-                        />
-                    )}
+                    <div style={{marginLeft: 'auto',}}>
+                        {this.props.userId ? (
+                            <GoogleLogout
+                            clientId={GOOGLE_CLIENT_ID}
+                            buttonText="Logout"
+                            onLogoutSuccess={this.props.handleLogout}
+                            onFailure={(err) => console.log(err)}
+                            className="NavBar-link NavBar-login"
+                            />
+                        ) : (
+                            <GoogleLogin
+                            clientId={GOOGLE_CLIENT_ID}
+                            buttonText="Login"
+                            onSuccess={this.props.handleLogin}
+                            onFailure={(err) => console.log(err)}
+                            className="NavBar-link NavBar-login"
+                            />
+                        )}
+                    </div>
                 </div>
             </nav>
         );
