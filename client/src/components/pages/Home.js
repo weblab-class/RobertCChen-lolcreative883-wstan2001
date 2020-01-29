975146ba44a740a0f7e2937e0cc770229c7e1c7f
lolcreative883 <59575036+lolcreative883@users.noreply.github.com>
2020-01-29 05:45:56 -0500

    added animation to home page with intro typewriter text

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -8,11 +8,11 @@ import Image from "../../public/meltingplot.png";
 //DONE: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "1085650892312-g9vj8eqaif6iug3qu3863i47ebj2hg86.apps.googleusercontent.com";
 
-/** 
+/**
 * Props: from App
 * path: for web address
-* handleLogin: 
-* handleLogout: 
+* handleLogin:
+* handleLogout:
 * userId: user's ID, from App
 */
 class Home extends Component {
@@ -29,11 +29,38 @@ class Home extends Component {
   render() {
     return (
       <>
-        <h1 className="Title">Welcome.</h1>
-        <img src={Image} className="Logo"/>
+      <center><h1 className="HomeTitle">Welcome.</h1></center>
+      <img src={Image} className="Logo"/>
+      <h1 className="HomeTitle2">Hey, Author!</h1>
+      <center>
+      <div class="typewriter">
+        <div class="typewriter-text">Welcome to Melting Plot, a choose-your-own-adventure-esque, story-writing collaboration platform you never knew you needed!</div>
+      </div>
+
+  <p></p>
+
+      <div class="typewriter">
+        <div class="typewriter-text2">We, Team Git Bashed, created this website to give people all around the world a chance to come together and volunteer their creative spirit, one step at a time!</div>
+      </div>
+
+  <p></p>
+
+      <div class="typewriter">
+        <div class="typewriter-text3">Create new stories! Inspire others! Raise awareness!</div>
+      </div>
+
+  <p></p>
+
+      <div class="typewriter">
+        <div class="typewriter-text4">The pen is in your hands... start creating! Hit the Browse button to look through stories or start your own!</div>
+      </div>
+
+      </center>
+
       </>
     );
   }
 }
+
 
 export default Home;
